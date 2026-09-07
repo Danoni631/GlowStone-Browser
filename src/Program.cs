10486c8a4ad80931bd2c097f0406811295311245
DaniloSenseiPlus <danunisonsoi@gmail.com>
2026-09-07 20:30:47 -0300

    Add XML documentation to Program class
    Added XML documentation for the main entry point.

--- src/Program.cs
+++ src/Program.cs
@@ -1,19 +1,21 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Glowstone
+using Glowstone;
+
+internal static class Program
 {
-    internal static class Program
+    /// <summary>
+    /// Ponto de entrada principal para o aplicativo.
+    /// </summary>
+    [STAThread]
+    static void Main()
     {
-        [STAThread]
-        static void Main()
-        {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
-        }
+        Application.EnableVisualStyles();
+        Application.SetCompatibleTextRenderingDefault(false);
+        Application.Run(new Form1());
     }
 }
