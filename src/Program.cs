80b9e6cc5c418ae4d522f488690a418c653cf492
DaniloSenseiPlus <danunisonsoi@gmail.com>
2026-09-15 21:55:35 -0300

    Modify Program to run Form2 after Form1
    Added a new method to run Form2 after a delay.

--- src/Program.cs
+++ src/Program.cs
@@ -1,21 +1,30 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-using Glowstone;
-
-internal static class Program
+namespace GlowStone
 {
-    /// <summary>
-    /// Ponto de entrada principal para o aplicativo.
-    /// </summary>
-    [STAThread]
-    static void Main()
+    internal static class Program
     {
-        Application.EnableVisualStyles();
-        Application.SetCompatibleTextRenderingDefault(false);
-        Application.Run(new Form1());
+        /// <summary>
+        /// Ponto de entrada principal para o aplicativo.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+            RunForm2();
+        }
+
+        static void RunForm2()
+        {
+            Thread.Sleep(1000);
+            Application.Run(new Form2());
+        }
     }
 }
